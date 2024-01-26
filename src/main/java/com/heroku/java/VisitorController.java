4eca57c7d989cf4248e4847e298bc321c4c8cc63
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-26 23:48:05 +0200

    add functionality to test the database

--- src/main/java/com/heroku/java/VisitorController.java
+++ src/main/java/com/heroku/java/VisitorController.java
@@ -7,18 +7,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/visitors")
 @RequiredArgsConstructor
 public class VisitorController {
-
+    private final DataSource dataSource;
     private VisitorService visitorService;
 
     @GetMapping
-    public ResponseEntity<List<Visitor>> getAllVisitors(){
-        return new ResponseEntity<List<Visitor>>(visitorService.allVisitors(),HttpStatus.OK);
+    String database(Map<String, Object> model) {
+        try (Connection connection = dataSource.getConnection()) {
+            final var statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
+
+            final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
+            final var output = new ArrayList<>();
+            while (resultSet.next()) {
+                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
+            }
+
+            model.put("records", output);
+            return "database";
+
+        } catch (Throwable t) {
+            model.put("message", t.getMessage());
+            return "error";
+        }
+//        return new ResponseEntity<List<Visitor>>(visitorService.allVisitors(),HttpStatus.OK);
     }
 
     @GetMapping("1")
