86d74d4d9f99259bcabc7370e1d84dda4ba850e3
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-27 00:52:53 +0200

    use Jpa for CRUD operations

--- src/main/java/com/heroku/java/VisitorController.java
+++ src/main/java/com/heroku/java/VisitorController.java
@@ -7,43 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/visitors")
 @RequiredArgsConstructor
 public class VisitorController {
-    private final DataSource dataSource;
+
     private VisitorService visitorService;
 
     @GetMapping
-    public ResponseEntity<List<Visitor>> database() {
-//        try (Connection connection = dataSource.getConnection()) {
-//            final var statement = connection.createStatement();
-//
-//            final var resultSet = statement.executeQuery("SELECT * FROM visitors");
-//            final var output = new ArrayList<>();
-//            while (resultSet.next()) {
-//                output.add(resultSet);
-//            }
-//
-//            model.put("records", output);
-            return new ResponseEntity<List<Visitor>>(visitorService.allVisitors(), HttpStatus.OK);
-
-//        } catch (Throwable t) {
-////            model.put("message", t.getMessage());
-//            return new ResponseEntity<List<Visitor>>(visitorService.allVisitors(), HttpStatus.BAD_REQUEST);
-//        }
-//        return new ResponseEntity<List<Visitor>>(visitorService.allVisitors(),HttpStatus.OK);
-    }
-
-    @GetMapping("1")
-    public ResponseEntity<List<Visitor>> getVisitors(){
-        System.out.println("There is nothing here!");
+    public ResponseEntity<List<Visitor>> getAllVisitors(){
         return new ResponseEntity<List<Visitor>>(visitorService.allVisitors(),HttpStatus.OK);
     }
+
 }
