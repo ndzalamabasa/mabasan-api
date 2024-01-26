9347389183b4d2fcea4ea0d43280eb001af370b7
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-26 21:26:37 +0200

    add functinality to enable service access

--- src/main/java/com/heroku/java/VisitorController.java
+++ src/main/java/com/heroku/java/VisitorController.java
@@ -1,6 +1,7 @@
 package com.heroku.java;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class VisitorController {
+    @Autowired
     private final VisitorService visitorService;
 
     @GetMapping("/visitors")
