fbd799aefe85bd53ca012e25312310966f4cd5ae
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-26 21:07:16 +0200

    add configuration bean

--- src/main/java/com/heroku/java/DatabaseConfiguration.java
+++ src/main/java/com/heroku/java/DatabaseConfiguration.java
@@ -1,2 +1,20 @@
-package com.heroku.java;public class DatabaseConfiguration {
+package com.heroku.java;
+
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
 }
