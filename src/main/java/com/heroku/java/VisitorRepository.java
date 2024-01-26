b7cfcff3df3c1b730a59c3240caee25ce10008c0
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-26 20:10:25 +0200

    remove funtionality that breaks the app

--- src/main/java/com/heroku/java/VisitorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.heroku.java;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-interface VisitorRepository extends JpaRepository<Visitor, UUID> {
-}
