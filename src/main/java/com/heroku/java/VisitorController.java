f0af1d58c40838b35e497bd98693a8d4ceac5e11
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-26 21:08:14 +0200

    add controller layer

--- src/main/java/com/heroku/java/VisitorController.java
+++ src/main/java/com/heroku/java/VisitorController.java
@@ -1,2 +1,22 @@
-package com.heroku.java;public class VisitorController {
+package com.heroku.java;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class VisitorController {
+    private final VisitorService visitorService;
+
+    @GetMapping("/visitors")
+    public ResponseEntity<List<Visitor>> getAllVisitors(){
+        return new ResponseEntity<List<Visitor>>(visitorService.allVisitors(),HttpStatus.OK);
+    }
 }
