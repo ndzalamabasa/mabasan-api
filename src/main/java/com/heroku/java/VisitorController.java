b23aad76eba4cc6ad9618f237080b7a0140f19a3
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-27 10:09:27 +0200

    change implementation for readability

--- src/main/java/com/heroku/java/VisitorController.java
+++ src/main/java/com/heroku/java/VisitorController.java
@@ -1,6 +1,6 @@
 package com.heroku.java;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/visitors")
-@RequiredArgsConstructor
+@RequestMapping("/app")
 public class VisitorController {
 
-    private VisitorService visitorService;
+    private final VisitorService visitorService;
 
-    @GetMapping
-    public ResponseEntity<List<Visitor>> getAllVisitors(){
-        return new ResponseEntity<List<Visitor>>(visitorService.allVisitors(),HttpStatus.OK);
+    @Autowired
+    public VisitorController(VisitorService visitorService) {
+        this.visitorService = visitorService;
     }
 
+    @GetMapping("/")
+    public ResponseEntity<List<Visitor>> getAllVisitors() {
+        List<Visitor> visitors = visitorService.allVisitors();
+        return new ResponseEntity<>(visitors, HttpStatus.OK);
+    }
 }
