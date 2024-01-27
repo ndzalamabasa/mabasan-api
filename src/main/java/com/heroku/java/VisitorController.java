e037166a65bb5e9536c0b469977a950260a2e712
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-27 10:48:51 +0200

    add post mapping test

--- src/main/java/com/heroku/java/VisitorController.java
+++ src/main/java/com/heroku/java/VisitorController.java
@@ -1,9 +1,13 @@
 package com.heroku.java;
 
+import java.util.Map;
+
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +30,12 @@ public class VisitorController {
     public ResponseEntity<String> getAllVisitors() {
 
         return new ResponseEntity<>("Hello app", HttpStatus.OK);
+    }
+
+    @PostMapping("/addVisitor")
+    public String addVisitor(@RequestBody Map<String, Object> body) {
+        String visitorName = (String) body.get("visitorName");
+
+        return visitorName;
     }
 }
