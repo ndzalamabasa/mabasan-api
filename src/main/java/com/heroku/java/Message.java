e57e3d4be348820ecf8a7372e39e57d58d667298
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-27 13:40:47 +0200

    remove visitor id for testing the route

--- src/main/java/com/heroku/java/Message.java
+++ src/main/java/com/heroku/java/Message.java
@@ -6,14 +6,12 @@ import lombok.Setter;
 @Setter
 @Getter
 public class Message {
-    public Message(Integer messageId, Integer visitorId, String message) {
+    public Message(Integer messageId, String message) {
         this.messageId = messageId;
-        this.visitorId = visitorId;
         this.message = message;
     }
 
     private Integer messageId;
-    private Integer visitorId;
     private String message;
 
 }
