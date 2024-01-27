144d685321e6388b911936c2f257b053d654d0de
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-27 12:34:21 +0200

    complete addVisitor post route

--- src/main/java/com/heroku/java/VisitorController.java
+++ src/main/java/com/heroku/java/VisitorController.java
@@ -1,8 +1,10 @@
 package com.heroku.java;
 
+import java.util.HashMap;
 import java.util.Map;
 
 // import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,18 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-// import java.util.List;
 
 @RestController
 @RequestMapping("/app")
 public class VisitorController {
 
-    // private final VisitorService visitorService;
-
-    // @Autowired
-    // public VisitorController(VisitorService visitorService) {
-    // this.visitorService = visitorService;
-    // }
+    @Autowired
+    VisitorService visitorService;
 
     @GetMapping
     @ResponseBody
@@ -33,9 +30,14 @@ public class VisitorController {
     }
 
     @PostMapping("/addVisitor")
-    public String addVisitor(@RequestBody Map<String, Object> body) {
+    public ResponseEntity<Map<String, String>> saveVisitor(@RequestBody Map<String, Object> body) {
         String visitorName = (String) body.get("visitorName");
+        String visitorEmail = (String) body.get("visitorEmail");
 
-        return visitorName;
+        Visitor visitor = visitorService.addVisitor(visitorName, visitorEmail);
+        Map <String, String> map = new HashMap<>();
+        map.put("message", "visitor added successfully");
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 }
