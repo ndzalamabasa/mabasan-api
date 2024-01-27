021fb542cd30291a65b348343c61e864778ca338
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-27 13:35:37 +0200

    remove unnecessary whitespace

--- src/main/java/com/heroku/java/MessageRepoImplementation.java
+++ src/main/java/com/heroku/java/MessageRepoImplementation.java
@@ -10,7 +10,7 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class MessageRepoImplementation implements MessageRepo{
-    private static final String INSERT_MESSAGE = "INSERT INTO messages(message) VALUES ( ?)";
+    private static final String INSERT_MESSAGE = "INSERT INTO messages(message) VALUES (?)";
 
     @Autowired
     JdbcTemplate jdbcTemplate;
