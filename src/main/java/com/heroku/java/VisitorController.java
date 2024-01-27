ef8ae2fd8e6e5eb5b8265cde29f80277de4d41e4
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-27 10:14:01 +0200

    add route test

--- src/main/java/com/heroku/java/VisitorController.java
+++ src/main/java/com/heroku/java/VisitorController.java
@@ -1,30 +1,29 @@
 package com.heroku.java;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+// import java.util.List;
 
 @RestController
 @RequestMapping("/app")
 public class VisitorController {
 
-    private final VisitorService visitorService;
+    // private final VisitorService visitorService;
 
-    @Autowired
-    public VisitorController(VisitorService visitorService) {
-        this.visitorService = visitorService;
-    }
+    // @Autowired
+    // public VisitorController(VisitorService visitorService) {
+    // this.visitorService = visitorService;
+    // }
 
     @GetMapping("/")
     @ResponseBody
-    ResponseEntity<List<Visitor>> getAllVisitors() {
-        List<Visitor> visitors = visitorService.allVisitors();
-        return new ResponseEntity<>(visitors, HttpStatus.OK);
+    String home() {
+        return "App";
     }
 }
