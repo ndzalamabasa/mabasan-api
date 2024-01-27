e354e9a88bafe872bccd573e64864432f35198ea
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-27 10:12:10 +0200

    add response body

--- src/main/java/com/heroku/java/VisitorController.java
+++ src/main/java/com/heroku/java/VisitorController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,7 +22,8 @@ public class VisitorController {
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<Visitor>> getAllVisitors() {
+    @ResponseBody
+    ResponseEntity<List<Visitor>> getAllVisitors() {
         List<Visitor> visitors = visitorService.allVisitors();
         return new ResponseEntity<>(visitors, HttpStatus.OK);
     }
