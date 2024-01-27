6625d977f4635b04c7f42894fc4040cfd3d014ae
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-27 20:14:33 +0200

    add email pattern validation

--- src/main/java/com/heroku/java/services/VisitorServiceImplementation.java
+++ src/main/java/com/heroku/java/services/VisitorServiceImplementation.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.regex.Pattern;
+
 @Service
 @Transactional
 public class VisitorServiceImplementation implements VisitorService {
@@ -16,10 +18,23 @@ public class VisitorServiceImplementation implements VisitorService {
 
     @Override
     public Visitor addVisitor(String visitorName, String visitorEmail) throws EtAuthException {
-        Integer visitorId = visitorRepository.addVisitor(visitorName, visitorEmail);
+        Pattern emailPattern = Pattern.compile("^[a-zA-Z0-9. _-]+@[a-zA-Z0-9. -]+\\.[a-zA-Z]$");
+
+        if(visitorEmail != null){
+            if(!emailPattern.matcher(visitorEmail).matches()){
+                throw new EtAuthException("Invalid email, try again.");
+            }
+
+            visitorEmail = visitorEmail.toLowerCase();
+        }
+
         if(visitorName == null || visitorEmail == null){
             throw new EtAuthException("required fields(*) missing.");
         }
+        
+        Integer visitorId = visitorRepository.addVisitor(visitorName, visitorEmail);
+
+
         return visitorRepository.getVisitorById(visitorId);
     }
 }
