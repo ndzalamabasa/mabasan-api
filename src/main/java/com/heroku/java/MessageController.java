bd8f9756d9e0327635a358bd5a11d8c46c767c93
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-27 13:26:48 +0200

    add post route for messages

--- src/main/java/com/heroku/java/MessageController.java
+++ src/main/java/com/heroku/java/MessageController.java
@@ -1,2 +1,30 @@
-package com.heroku.java;public class MessageController {
+package com.heroku.java;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/messages")
+public class MessageController {
+
+    @Autowired
+    MessageService messageService;
+    @PostMapping
+    public ResponseEntity<Map<String, String>> saveMessage(@RequestBody Map<String, Object> body) {
+        String message = (String) body.get("message");
+
+        Message newMessage = messageService.saveMessage(message);
+        Map<String, String> map = new HashMap<>();
+        map.put("success", "message was sent");
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
 }
