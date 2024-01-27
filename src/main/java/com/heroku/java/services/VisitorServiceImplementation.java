51959114f6833dd448a01183145b9d40e97ac7c3
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-27 20:18:18 +0200

    remove data validation

--- src/main/java/com/heroku/java/services/VisitorServiceImplementation.java
+++ src/main/java/com/heroku/java/services/VisitorServiceImplementation.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.regex.Pattern;
-
 @Service
 @Transactional
 public class VisitorServiceImplementation implements VisitorService {
@@ -18,23 +16,7 @@ public class VisitorServiceImplementation implements VisitorService {
 
     @Override
     public Visitor addVisitor(String visitorName, String visitorEmail) throws EtAuthException {
-        Pattern emailPattern = Pattern.compile("^[a-zA-Z0-9. _-]+@[a-zA-Z0-9. -]+\\.[a-zA-Z]$");
-
-        if(visitorEmail != null){
-            if(!emailPattern.matcher(visitorEmail).matches()){
-                throw new EtAuthException("Invalid email, try again.");
-            }
-
-            visitorEmail = visitorEmail.toLowerCase();
-        }
-
-        if(visitorName == null || visitorEmail == null){
-            throw new EtAuthException("required fields(*) missing.");
-        }
-        
         Integer visitorId = visitorRepository.addVisitor(visitorName, visitorEmail);
-
-
         return visitorRepository.getVisitorById(visitorId);
     }
 }
