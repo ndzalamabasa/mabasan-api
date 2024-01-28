2f710aafc1917b4ca0e666814987c531ee8288c5
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-28 16:47:49 +0200

    add a domain to allow CORS for

--- src/main/java/com/heroku/java/controllers/VisitorController.java
+++ src/main/java/com/heroku/java/controllers/VisitorController.java
@@ -11,14 +11,9 @@ import com.heroku.java.entities.Visitor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = "http://localhost:5500")
 @RestController
 @RequestMapping("/api/v1/messages")
 public class VisitorController {
