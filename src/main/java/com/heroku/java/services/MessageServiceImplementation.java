d940440e5b47cc69bc1fd14c5149a20b6616e557
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-01-28 23:04:42 +0200

    remove unused package

--- src/main/java/com/heroku/java/services/MessageServiceImplementation.java
+++ src/main/java/com/heroku/java/services/MessageServiceImplementation.java
@@ -2,7 +2,6 @@ package com.heroku.java.services;
 
 import com.heroku.java.exceptions.ValidateDataException;
 import com.heroku.java.repositories.MessageRepo;
-import com.heroku.java.exceptions.EtAuthException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
